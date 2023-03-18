ab1fca15d6571bef67001ebb8278c928e4e8a9a1
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-03-18 22:59:48 +0530

    Landing Page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
+import NotFound from "./components/404/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    
-    
-    
     </BrowserRouter>
   );
 }
