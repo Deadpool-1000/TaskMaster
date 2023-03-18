88d3fc5cad4e0aaa744b886c877b6e135f7e4ad2
Milind <milibhatnagar2002@gmail.com>
2023-03-19 01:36:20 +0530

    Added task logic

--- Frontend/src/components/Form/Form.js
+++ Frontend/src/components/Form/Form.js
@@ -5,18 +5,23 @@ import InputForm from "./InputForm";
 
 function Form() {
   const [taskList, setTaskList] = useState([
-    { title: "HomeWork", deadline: "tomorrow evening" },
-    { title: "Project", deadline: "today 9" },
+    { id:1, title: "HomeWork", deadline: "tomorrow evening" },
+    { id:2,title: "Project", deadline: "today 9" },
   ]);
   function addTask(task) {
-    setTaskList([...taskList, task]);
+    setTaskList([...taskList, {...task,id:taskList.length+1}]);
+  }
+  function deleteTask(_id){
+    setTaskList((taskList)=>{
+      return taskList.filter((task)=>task.id!==_id)
+    })
   }
   return (
     <div className={`container mx-auto pt-32 p-64`}>
-      <InputForm addTask={addTask} />
-      <ul class="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mt-10 ">
-        {taskList.map((task) => {
-          return <Task title={task.title} deadline={task.deadline} />;
+      <InputForm addTask={addTask}/>
+      <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mt-10 ">
+        {taskList.map((task,i) => {
+          return <Task id={task.id} key={i} title={task.title} deadline={task.deadline} deleteTask={deleteTask} />;
         })}
       </ul>
     </div>
