675716cab8c89ce1ef0a655d1b536957f6a9de41
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-03-19 01:37:06 +0530

    Minor Changes

--- Frontend/src/components/Form/Form.js
+++ Frontend/src/components/Form/Form.js
@@ -17,11 +17,19 @@ function Form() {
     })
   }
   return (
+<<<<<<< HEAD
     <div className={`container mx-auto pt-32 p-64`}>
       <InputForm addTask={addTask}/>
       <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mt-10 ">
         {taskList.map((task,i) => {
           return <Task id={task.id} key={i} title={task.title} deadline={task.deadline} deleteTask={deleteTask} />;
+=======
+    <div className={`container mx-auto pt-32 md:pl-64 md:pr-64  sm:pr-32 sm:pl-32`}>
+      <InputForm addTask={addTask} />
+      <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mt-10 ">
+        {taskList.map((task) => {
+          return <Task title={task.title} deadline={task.deadline} />;
+>>>>>>> parent of 1ff5c62a (Revert "minor changes")
         })}
       </ul>
     </div>
